530766e9a20e4d7625cc9c7669adf628a530e69d
Rafael Delboni <rafadelboni@gmail.com>
2022-03-17 10:58:38 -0300

    fix: Enables malli on test run

--- src/parenthesin/utils.clj
+++ src/parenthesin/utils.clj
@@ -1,10 +1,11 @@
 (ns parenthesin.utils
-  (:require [malli.instrument :as mi]))
+  (:require [malli.dev.pretty :as pretty]
+            [malli.instrument :as mi]))
 
 (defn with-malli-intrumentation
   "Wraps f ensuring there has malli collect and instrument started before running it"
   [f]
-  (mi/collect!)
-  (mi/instrument!)
+  (mi/collect! {:ns (all-ns)})
+  (mi/instrument! {:report (pretty/reporter)})
   (f)
   (mi/unstrument!))
