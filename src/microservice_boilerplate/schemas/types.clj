9ab0562d74a6f5219e2cfa7f5919ef78a5e89596
Rafael Delboni <rafadelboni@gmail.com>
2023-04-05 23:47:15 -0300

    feat: uses malli time schemas

--- src/microservice_boilerplate/schemas/types.clj
+++ src/microservice_boilerplate/schemas/types.clj
@@ -1,9 +1,9 @@
 (ns microservice-boilerplate.schemas.types
   (:require [com.stuartsierra.component :as component]
             [malli.core :as m]
+            [malli.experimental.time :as met]
             [parenthesin.components.database :as components.database]
-            [parenthesin.components.http :as components.http])
-  (:import [java.time LocalDateTime ZoneId]))
+            [parenthesin.components.http :as components.http]))
 
 (def HttpComponent
   (m/-simple-schema
@@ -24,16 +24,10 @@
     :type-properties {:error/message "should satisfy com.stuartsierra.component/Lifecycle protocol."}}))
 
 (def JavaLocalDateTime
-  (m/-simple-schema
-   {:type :localdatetime
-    :pred #(instance? LocalDateTime %)
-    :type-properties {:error/message "should be an instance of LocalDateTime."}}))
+  (met/-local-date-time-schema))
 
 (def JavaZoneId
-  (m/-simple-schema
-   {:type :zone-id
-    :pred #(instance? ZoneId %)
-    :type-properties {:error/message "should be an instance of ZoneId."}}))
+  (met/-zone-id-schema))
 
 (def Components
   [:map
